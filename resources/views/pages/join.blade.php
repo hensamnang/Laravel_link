b991a6169475a6b41b4ab28f4947e4726f334e8f
hensamnang <samnang.hen@student.passerellesnumeriques.org>
2020-01-20 08:18:38 +0700

    loop array in array

--- resources/views/pages/join.blade.php
+++
@@ -1,3 +0,0 @@
-<h2>Join Us</h2>
-<p>We rely on the talents and dedicated human resources to develop our actions and
-sustain our activities.</p>
--- resources/views/pages/news.blade.php
+++
@@ -1,2 +0,0 @@
-<h2>News</h2>
-<p>Find all the latest news, stories and unique insights from Passerelles numériques here!</p>
